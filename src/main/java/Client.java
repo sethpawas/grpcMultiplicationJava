aa9a6b77683b5cdc98b169aee8128bb37fb17a5f
pawas <sethpawas2950@gmail.com>
2019-06-07 11:52:04 +0530

    Containing Client Server

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,6 @@
-import com.proto.calculator.*;
+import com.proto.calculator.CalculatorServiceGrpc;
+import com.proto.calculator.FibonacciResponse;
+import com.proto.calculator.Request;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
@@ -17,105 +19,19 @@ public class Client {
         ManagedChannel ch = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext().build();
 
-        tables(ch);
-        greet(ch);
-        GCD(ch);
+        fibonacciSeries(ch);
 
         ch.shutdown();
     }
 
-    private void tables(ManagedChannel channel) {
+    private void fibonacciSeries(ManagedChannel channel) {
         CalculatorServiceGrpc.CalculatorServiceBlockingStub stub = CalculatorServiceGrpc.newBlockingStub(channel);
+        //Request req = Request.newBuilder().build();
+       Request req = Request.newBuilder().setNumber(23).build();
 
-        tableRequest req = tableRequest.newBuilder().setNumber(10).build();
-        tableResponse res = stub.tables(req);
+        FibonacciResponse res = stub.fibonacciSeries(req);
 
-        System.out.println("Cube: " + res.getResult());
-    }
 
-    private void greet(ManagedChannel channel) {
-        CalculatorServiceGrpc.CalculatorServiceBlockingStub stub = CalculatorServiceGrpc.newBlockingStub(channel);
-
-        greetingsRequest req = greetingsRequest.newBuilder().setA("ayush").build();
-
-        greetingsResponse res =stub.greet(req);
-//        stub.primeFactors(req).forEachRemaining(primeFactorResponse -> {
-//            System.out.println("Prime Factors: " + primeFactorResponse.getResult());
-//        });
-        System.out.println(res.getB());
-    }
-
-//    private void average(ManagedChannel channel) {
-//        CalculatorServiceGrpc.CalculatorServiceStub stub = CalculatorServiceGrpc.newStub(channel);
-//
-//        CountDownLatch latch = new CountDownLatch(1);
-//
-//        StreamObserver<TableRequest> reqObserver = stub.average(new StreamObserver<CalculatorAverageResponse>() {
-//            @Override
-//            public void onNext(CalculatorAverageResponse value) {
-//                System.out.println("Average: " + value.getResult());
-//            }
-//
-//            @Override
-//            public void onError(Throwable t) {
-//
-//            }
-//
-//            @Override
-//            public void onCompleted() {
-//                System.out.println("Finished");
-//                latch.countDown();
-//            }
-//        });
-//
-//        for(int i = 0; i < 10000; i++) {
-//            reqObserver.onNext(TableRequest.newBuilder()
-//                    .setNumber(i).build());
-//        }
-//
-//        reqObserver.onCompleted();a
-//
-//        try {
-//            latch.await(3, TimeUnit.SECONDS);
-//        } catch(InterruptedException ex) {
-//            ex.printStackTrace();
-//        }
-//    }
-
-    private void GCD(ManagedChannel channel) {
-        CalculatorServiceGrpc.CalculatorServiceStub stub = CalculatorServiceGrpc.newStub(channel);
-
-        CountDownLatch latch = new CountDownLatch(1);
-
-        StreamObserver<tableRequest> reqObserver = stub.gCD(new StreamObserver<divisorResponse>() {
-            @Override
-            public void onNext(divisorResponse value) {
-                System.out.println("Number: " + value.getNum());
-            }
-
-            @Override
-            public void onError(Throwable t) {
-
-            }
-
-            @Override
-            public void onCompleted() {
-                System.out.println("Finished");
-                latch.countDown();
-            }
-        });
-
-        for(int i = 0; i < 10; i++) {
-            reqObserver.onNext(tableRequest.newBuilder()
-                    .setNumber(i).build());
-        }
-
-        reqObserver.onCompleted();
-
-        try {
-            latch.await(3, TimeUnit.SECONDS);
-        } catch(InterruptedException ex) {
-            ex.printStackTrace();
-        }
+        System.out.println("Multiplication is: " + res.getResult());
     }
 }
